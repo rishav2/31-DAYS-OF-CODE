7ca6ce607f477a8ad86df0ca8f501000e1a96d60
rishav2 <41112605+rishav2@users.noreply.github.com>
2019-01-21 11:51:00 +0530

    .java

---
+++ DAY 5/solution.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+import javax.lang.model.util.ElementScanner6;
+
+public class Person {
+    private int age;	
+  
+	public Person(int initialAge) {
+        if(initialAge<0)
+        {
+            this.age= 0;
+            System.out.println("Age is not valid, setting age to 0.");
+        }
+       else
+       {
+           this.age= initialAge;
+       }
+  		
+	}
+     public void amIOld() {
+  		if (this.age < 13) {
+            System.out.println("You are young.");
+
+        } else if (this.age >= 13 && this.age < 18) {
+            System.out.println("You are a teenager.");
+        } else {
+        
+            System.out.println("You are old.");
+        }
+        
+    
+	}
+
+	public void yearPasses() {
+  	        this.age++;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int i = 0; i < T; i++) {
+			int age = sc.nextInt();
+			Person p = new Person(age);
+			p.amIOld();
+			for (int j = 0; j < 3; j++) {
+				p.yearPasses();
+			}
+			p.amIOld();
+			System.out.println();
+        }
+		sc.close();
+    }
+}
